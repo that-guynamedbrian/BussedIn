dd50f5dc038a874e74415bf146fde80846ce3c0e
that-guynamedbrian <vpnalt67@gmail.com>
2025-12-31 12:31:51 +0600

    auto-commit

--- src/ServerStorage/AyoFramework/Types.emmylua.lua
+++ src/ServerStorage/AyoFramework/Types.emmylua.lua
@@ -7,9 +7,15 @@
 ---@class CharacterAyo : AyoUnit
 ---@field Instance Model
 ---@field AyoType "Character"
----@field Inventory table<number,PickupableAyo>
+---@field Inventory table<string, table<string, PickupableAyo>> { ayoKey : {unitKey:PickupableAyo} }
+---@field Pickup fun(self:CharacterAyo, toPickup:PickupableAyo)
+---@field Equip fun(self:CharacterAyo, toEquip:PickupableAyo)
+---@field Unequip fun(self:CharacterAyo, toUnequip:PickupableAyo)
+---@field Remove fun(self:CharacterAyo, count:table<string,number>) count: {ayoKey: numberToRemove}
+---@
 
 ---@type CharacterAyo
 local a = {}
-local b = a.Inventory
+local b =
+local d =
 return nil

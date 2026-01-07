9ed821f6f3f95ceba302e6d87907ea57e5abc5ff
that-guynamedbrian <vpnalt67@gmail.com>
2026-01-07 16:11:49 +0600

    auto-commit

--- src/ReplicatedStorage/UIModules/MainHUD/init.lua
+++ src/ReplicatedStorage/UIModules/MainHUD/init.lua
@@ -1,12 +1,13 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
 local LeftSlot = require(script.LeftSlot)
 local React = require(ReplicatedStorage.Packages.React)
-local ReactRoblox = require(ReplicatedStorage.Packages.ReactRoblox)
 
 
-return React.createElement("ScreenGui", {}, {
-	LeftButtonsFrame = LeftSlot(),
-	SlotFrame = Hotbar(),
-	TopRightFrame = TopRight(),
-})
+return function()
+	return React.createElement("ScreenGui", {
+	}, {
+		LeftButtonsFrame = React.createElement(LeftSlot),--[[
+		SlotFrame = Hotbar(),
+		TopRightFrame = TopRight(),]]
+	})
+end
--- src/ReplicatedStorage/UIModules/hud.story.lua
+++ src/ReplicatedStorage/UIModules/hud.story.lua
@@ -1,10 +1,15 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Roact = require(ReplicatedStorage.Packages.roact)
+local MainHUD = require(script.Parent.MainHUD)
+local React = require(ReplicatedStorage.Packages.React)
+local ReactRoblox = require(ReplicatedStorage.Packages.ReactRoblox)
 
-return function()
-    local root = require(script.Parent.MainHUD)
-    root:render()
+
+return function(parent)
+    local root = ReactRoblox.createRoot(parent)
+    local app = React.createElement("ScreenGui",{})
+    root:render(app)
+
     return function()
-        
+        root:unmount()
     end
 end
